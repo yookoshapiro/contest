6c59aae914ad42227bc21206c6ee65065c7efe4b
Steven Markgraf <yooko.shapiro@gmail.com>
2023-03-12 03:57:26 +0100

    Validation für Benutzer-Angaben

--- lib/namespace/Contest/Controller/UserController.php
+++ lib/namespace/Contest/Controller/UserController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Contest\Controller;
 
+use Cake\Validation\Validator;
 use Slim\Routing\RouteCollectorProxy;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
@@ -42,11 +43,6 @@ final class UserController
      */
     public function create(Request $request, Response $response): Response
     {
-
-        $response->getBody()->write(json_encode([
-            'id' => md5( (string) time() ),
-            'create' => true
-        ]));
 
         return $response
             ->withStatus(201);
@@ -95,10 +91,125 @@ final class UserController
     public static function router(RouteCollectorProxy $group): void
     {
 
-        $group->post('', [self::class, 'create']);
-        $group->get('/{id}', [self::class, 'show']);
-        $group->patch('/{id}', [self::class, 'edit']);
-        $group->delete('/{id}', [self::class, 'remove']);
+        $group->post('', [self::class, 'create'])->add( self::getValidationMiddleware(true) );
+        $group->get('/{id}', [self::class, 'show'])->add( self::getValidationMiddleware() );
+        $group->patch('/{id}', [self::class, 'edit'])->add( self::getValidationMiddleware() );
+        $group->delete('/{id}', [self::class, 'remove'])->add( self::getValidationMiddleware() );
+
+    }
+
+
+    /**
+     * Gibt eine Middleware zum Validieren der Benutzereingaben wieder.
+     *
+     * @param bool $newRecord
+     * @return callable
+     */
+    public static function getValidationMiddleware(bool $newRecord = false): callable
+    {
+
+        return function($request, $handler) use ($newRecord)
+        {
+
+            $post = $request->getParsedBody();
+            $errors = self::dataValidation($post, $newRecord);
+
+            if (count($errors) !== 0)
+            {
+
+                $response = new \Slim\Psr7\Response();
+                $response->getBody()->write( json_encode($errors) );
+
+                return $response
+                    ->withStatus(400);
+
+            }
+
+            return $handler->handle($request);
+
+        };
+
+    }
+
+
+    /**
+     * Validiert die Eingaben zu einem Benutzer und gibt alle Fehler als Array zurück
+     *
+     * @param array $data
+     * @param bool $newRecord [optional] default: true - true, wenn neuer Eintrag
+     *                                                   false, wenn Eintrag aktualisiert wird
+     * @return array
+     */
+    protected static function dataValidation(array $data, bool $newRecord = true): array
+    {
+
+        $validator = new Validator();
+
+        $validator
+            ->requirePresence('id', 'update', 'Die ID wird benötigt.')
+            ->add('id', 'id_length', [
+                'rule' => function($value, $context)
+                {
+
+                    if ($context['newRecord'] === true or strlen($value) === 36) {
+                        return true;
+                    }
+
+                    return 'Die ID ist zu ' . (strlen($value) < 36 ? 'kurz.' : 'lang.');
+
+                }
+            ]);
+
+        $validator
+            ->requirePresence('phrase', false)
+            ->add('phrase', 'phrase_length', [
+            'rule' => function($value, $context)
+            {
+
+                if ($context['newRecord'] === true or strlen($value) === 8) {
+                    return true;
+                }
+
+                return 'Die Phrase ist zu ' . (strlen($value) < 8 ? 'kurz.' : 'lang.');
+
+            }
+        ]);
+
+        $validator
+            ->requirePresence('name', 'create', 'Der Name wird benötigt.')
+            ->notEmptyString('name', 'Der Name darf nicht leer sein.')
+            ->add('name', [
+                'length' => [
+                    'rule' => ['minLength', 3],
+                    'message' => 'Der Name muss min. 3 Zeichen lang sein.'
+                ]
+            ]);
+
+        $validator
+            ->requirePresence('password', 'create', 'Ein Passwort wird benötigt.')
+            ->notEmptyString('password', 'Das Passwort darf nicht leer sein.')
+            ->add('password', [
+                'length' => [
+                    'rule' => ['minLength', 8],
+                    'message' => 'Das Passwort muss min. 8 Zeichen lang sein.'
+                ]
+            ]);
+
+        $validator
+            ->requirePresence('email', false)
+            ->notEmptyString('email', 'Die E-Mail darf nicht leer sein.')
+            ->add('email', "email_valid", [
+                'rule' => "email",
+                'message' => 'Es muss eine gültige E-Mail sein.'
+            ]);
+
+        $validator
+            ->requirePresence('active', false)
+            ->add('active', 'active_type', [
+                'rule' => 'boolean'
+            ]);
+
+        return $validator->validate($data, $newRecord);
 
     }
 
--- lib/namespace/Contest/Database/User.php
+++ lib/namespace/Contest/Database/User.php
@@ -5,8 +5,11 @@ declare(strict_types=1);
 namespace Contest\Database;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class User extends Model
 {
+
+    use HasUuids;
 
 }
