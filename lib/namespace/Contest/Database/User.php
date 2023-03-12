0aecf8aece9cc9c9b6f328f6cacfad5c21bab8eb
Steven Markgraf <yooko.shapiro@gmail.com>
2023-03-12 12:38:32 +0100

    Ulids nutzen und Passwort standardmäßig nicht ausgeben

--- lib/namespace/Contest/Database/User.php
+++ lib/namespace/Contest/Database/User.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace Contest\Database;
 
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Concerns\HasUuids;
 
 class User extends Model
 {
 
-    use HasUuids;
+    # Aktiviere Ulids als PrimaryKey
+    use HasUlids;
+
+    protected $hidden = ['password'];
 
 }
