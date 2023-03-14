cfba57b86367bcade83d045a02c9c64e05d13da0
Steven Markgraf <yooko.shapiro@gmail.com>
2023-03-14 23:41:46 +0100

    Exception für EntryMiddleware | fromBody liesst Attribute aus dem Body aus

--- lib/namespace/Contest/Middleware/Api/EntryMiddleware.php
+++ lib/namespace/Contest/Middleware/Api/EntryMiddleware.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Contest\Middleware\Api;
 
+use Contest\Exception\EntryMiddlewareException;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
@@ -56,18 +57,73 @@ class EntryMiddleware
             try
             {
 
+                $model = call_user_func([$class, 'find'], $id);
+
+                if ($model === null) {
+                    throw new EntryMiddlewareException;
+                }
+
                 return $handler->handle(
                     $request->withAttribute($name, call_user_func([$class, 'findOrFail'], $id))
                 );
 
             }
-            catch (\Exception $ex)
+            catch (EntryMiddlewareException $ex)
             {
 
                 $response = new \Slim\Psr7\Response;
 
                 $response->getBody()->write(json_encode([
                     'error' => "no {$name} with id '{$id}' found"
+                ]));
+
+                return $response
+                    ->withStatus(404);
+
+            }
+
+        };
+
+    }
+
+
+    /**
+     * Liest das Attribut aus dem Body aus.
+     *
+     * @param string $class
+     * @param string $attributeName
+     * @return callable
+     */
+    public static function fromBody(string $class, string $attributeName): callable
+    {
+
+        return function(Request $request, RequestHandler $handler) use ($class, $attributeName): Response
+        {
+
+            $post = $request->getParsedBody();
+            $name = self::extractName($class);
+
+            try
+            {
+
+                $model = call_user_func([$class, 'find'], $post[ $attributeName ]);
+
+                if ($model === null) {
+                    throw new EntryMiddlewareException;
+                }
+
+                return $handler->handle(
+                    $request->withAttribute($name, $model)
+                );
+
+            }
+            catch (EntryMiddlewareException $ex)
+            {
+
+                $response = new \Slim\Psr7\Response;
+
+                $response->getBody()->write(json_encode([
+                    'error' => "no {$name} with id '{$post[$attributeName]}' found"
                 ]));
 
                 return $response
