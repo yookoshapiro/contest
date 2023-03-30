77cfc2147d4b6a213c11c6e97978470c5936d178
Steven Markgraf <yooko.shapiro@gmail.com>
2023-03-30 03:10:25 +0200

    CustomAlert vereinfacht die Nutzung

---
+++ lib/vue/src/components/elements/CustomAlert.vue
@@ -0,0 +1,24 @@
+<template>
+  <Teleport to="#message-custom" v-if="active">
+    <slot />
+  </Teleport>
+</template>
+
+<script setup lang="ts">
+
+import { ref } from 'vue';
+
+interface Props {
+  active: boolean
+}
+
+const props: Props = defineProps({
+  active: {
+    type: Boolean,
+    default: false
+  }
+});
+
+const readonly = ref(props.active);
+
+</script>
