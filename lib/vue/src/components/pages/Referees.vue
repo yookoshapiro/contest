caf5450af7b75e849e04da74c004987c90a42a59
Steven Markgraf <Steven.Markgraf@uksh.de>
2023-12-26 16:55:44 +0100

    neue Seite für Kampfrichter eingefügt

---
+++ lib/vue/src/components/pages/Referees.vue
@@ -0,0 +1,23 @@
+<template>
+
+  <div class="title">
+    <h3>Kampfrichter</h3>
+  </div>
+
+  <div class="content">
+    <div class="block">
+      <table>
+        <thead>
+        <td>Name</td>
+        <td>Station</td>
+        <td>Aktionen</td>
+        </thead>
+      </table>
+    </div>
+  </div>
+
+</template>
+
+<script setup lang="ts">
+
+</script>
