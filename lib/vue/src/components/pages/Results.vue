2f8c60f71347dc7537523dbbe485b53ad5b3811e
Steven Markgraf <Steven.Markgraf@uksh.de>
2023-12-27 01:30:01 +0100

    Grund-Tabelle für Ergebnisse

--- lib/vue/src/components/pages/Results.vue
+++ lib/vue/src/components/pages/Results.vue
@@ -1,13 +1,25 @@
 <template>
+
   <div class="title">
     <h3>Ergebnisse</h3>
   </div>
+
+  <div class="content">
+    <div class="block">
+      <table>
+        <thead>
+        <td style="width: 40px;">#</td>
+        <td>Ergebnis</td>
+        <td>Station</td>
+        <td>Wert</td>
+        <td>Aktionen</td>
+        </thead>
+      </table>
+    </div>
+  </div>
+
 </template>
 
 <script setup lang="ts">
 
 </script>
-
-<style scoped>
-
-</style>
